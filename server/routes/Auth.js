16a14e24bbf8e0a3995e91522ce591bd0f6099b3
Rupam Shil <rupamshil111@gmail.com>
2021-10-01 09:56:39 +0530

    Session Storage Added
    1)session storage added
    2)id is passed to store in vuex
    3)home route created

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/Userdetails');
-const { body, validationResult } = require('express-validator');
 
 router.post(
 	'/signup',
-	body('email').isEmail(),
-	body('password').isLength({ min: 6 }),
-	(req, res) => {
+	[
+		check('email', 'Please provide a valid email').isEmail(),
+		check('password', 'Please enter a strong password').isLength({ min: 6 }),
+	],
+	async (req, res) => {
 		const errors = validationResult(req);
 		const { name, email, password } = req.body;
+		if (errors.isEmpty()) {
+			const isUserExist = await User.findOne({ email });
+			if (!isUserExist) {
+				try {
+					const hashedPassword = await bcrypt.hash(password, 10);
+					const user = await User({
+						name: name,
+						email: email,
+						password: hashedPassword,
+					});
+					const newUser = await user.save();
+					res.json({
+						id: newUser.id,
+						name: newUser.name,
+						email: newUser.email,
+					});
+				} catch (err) {
+					console.log(err);
+					res.status(400).json({ error: 'An error occurred while saving' });
+				}
+			} else {
+				res.json({ error: 'User already exists' });
+			}
+		} else {
+			res.json({ error: errors.array()[0].msg });
+		}
 	}
 );
 
